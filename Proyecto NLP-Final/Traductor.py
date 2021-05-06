ccc56e2794fc8510b19708ad57cc9b4c78aeb4dc
jrm2087 <jrm2087@gmail.com>
2021-05-05 22:17:31 -0500

    Ajustes seleccion de idomas, funcion traductor y plugin

--- Proyecto NLP-Final/Traductor.py
+++ Proyecto NLP-Final/Traductor.py
@@ -1,10 +1,6 @@
 import time
 import bs4 as bs
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 lang_source = {
     "DE":"translator-lang-option-de",#alemán
@@ -32,7 +28,6 @@ lang_source = {
     "RU":"translator-lang-option-ru",#ruso
     "SV":"translator-lang-option-sv" #sueco
 }
-
 
 lang_to = {
     "DE":"translator-lang-option-de-DE",#alemán
@@ -62,44 +57,31 @@ lang_to = {
     "SV":"translator-lang-option-sv-SV" #sueco
 }
 
-
-def translater(translate,lan_source,lan_to):
+def translater(translate, lan_source="ES", lan_to="EN"):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
     url = 'https://www.deepl.com/es/translator'
-    browser = webdriver.PhantomJS('phantomjs-2.1.1-windows\\bin\\phantomjs.exe')
+    browser = webdriver.Chrome('plugin\\chromedriver', options=chrome_options)
     browser.get(url)
-    
-    #btn_lang_source = browser.find_elements_by_class_name("lmt__language_select--source")[0]    
-    btn_lang_source = WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.CLASS_NAME,"lmt__language_select--source")))
-    btn_lang_source.click()    
-    print(btn_lang_source)
-    time.sleep(3)
-    raw_html = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-    target = bs.BeautifulSoup(raw_html, 'html.parser').find(class_="lmt__language_select__menu")
-    
-    print(target)
-    WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,"//button[@dl-test='" + lang_source[lan_source] + "']")))
-    #browser.find_element_by_xpath("//button[@dl-test='" + lang_source[lan_source] + "']")
-    #browser.execute_script("document.getElementsByClassName('lmt__language_select__active')[0].click()")  
-    #browser.execute_script("document.querySelectorAll('[dl-test=\""+lang_source[lan_source]+"\"]')[0].click()")
-    
-    #browser.execute_script("document.getElementsByClassName('lmt__language_select__active')[1].click()")  
-    #browser.execute_script("document.querySelectorAll('[dl-test=\""+lang_to[lan_to]+"\"]')[0].click()")  
-    
-    time.sleep(3)
+    #-------------------- Seleccion de idioma --------------------
+    # lan_source
+    browser.execute_script("document.getElementsByClassName('lmt__language_select__active')[0].click()")  
+    browser.execute_script("document.querySelectorAll('[dl-test=\""+lang_source[lan_source]+"\"]')[0].click()")
+    # lan_to
+    browser.execute_script("document.getElementsByClassName('lmt__language_select__active')[1].click()")  
+    browser.execute_script("document.querySelectorAll('[dl-test=\""+lang_to[lan_to]+"\"]')[0].click()") 
+    #-------------------- Envia al textArea --------------------    
     input_area = browser.find_element_by_class_name('lmt__source_textarea')
     input_area.clear() 
     input_area.send_keys(translate)
-    time.sleep(3)
+    time.sleep(5)
     raw_html = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-    target = bs.BeautifulSoup(raw_html, 'html.parser').find(class_="lmt__textarea lmt__textarea_dummydiv", attrs={"id":"target-dummydiv"})
+    target = bs.BeautifulSoup(raw_html, 'html.parser').find(class_="lmt__textarea", attrs={"id":"target-dummydiv"})
     browser.quit()
     return target.text
 
-
-
-
 def main():
-    print(translater("Las aves son animales vertebrados, de sangre caliente, que caminan, saltan o se mantienen solo sobre las extremidades posteriores","ES", "EN"))
+    print(translater("Las aves son animales vertebrados, de sangre caliente, que caminan, saltan o se mantienen solo sobre las extremidades posteriores"))
 
 
 if __name__ == "__main__":
